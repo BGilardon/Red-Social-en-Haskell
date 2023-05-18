eace45f26c087e2974d15fd14f0a72f7abe0945c
bautista-gilardon <bautistagilardon@gmail.com>
2023-05-18 15:47:45 -0300

    Test

--- Solucion.hs
+++ Solucion.hs
@@ -1,12 +1,14 @@
+module Solucion where
+
 import Test.HUnit
 
 -- Completar con los datos del grupo
 --
--- Nombre de Grupo: xx
--- Integrante 1: Nombre Apellido, email, LU
--- Integrante 2: Nombre Apellido, email, LU
--- Integrante 3: Nombre Apellido, email, LU
--- Integrante 4: Nombre Apellido, email, LU
+-- Nombre de Grupo: Fila3
+-- Integrante 1: Bautista Gilardon, bautistagilardon@gmail.com, 742/21
+-- Integrante 2: Juan Pablo Gervasi, gervasi.juanpablo@gmail.com, 499/21
+-- Integrante 3: Carolina Yañez, carolina.m.yanez@gmail.com, 425/20
+-- Integrante 4: Franco Pomi, pomifranco@gmail.com, 935/22
 
 type Usuario = (Integer, String) -- (id, nombre)
 type Relacion = (Usuario, Usuario) -- usuarios que se relacionan
@@ -38,9 +40,7 @@ likesDePublicacion (_, _, us) = us
 
 -- Ejercicios
 
-pertenece :: (Eq x) => x -> [x] -> Bool
-pertenece _ [] = False
-pertenece y (x:xs) = y == x || pertenece y xs 
+
 
 -- describir qué hace la función: .....
 nombresDeUsuarios :: RedSocial -> [String]
@@ -112,7 +112,9 @@ publicacionesQueLeGustanAAux [] _ = []
 publicacionesQueLeGustanAAux (p:ps) us  | pertenece us (likesDePublicacion p) = p : (publicacionesQueLeGustanAAux ps us)
                                         | otherwise = (publicacionesQueLeGustanAAux ps us)
 
-
+pertenece :: (Eq x) => x -> [x] -> Bool
+pertenece _ [] = False
+pertenece y (x:xs) = y == x || pertenece y xs 
 
 -- describir qué hace la función: .....
 lesGustanLasMismasPublicaciones :: RedSocial -> Usuario -> Usuario -> Bool
@@ -157,7 +159,7 @@ existeSecuenciaDeAmigosAux [] _ _ = False
 existeSecuenciaDeAmigosAux (r:rs) u1 u2 | not (tieneAmigos u1 (r:rs))                   = False                                         -- Si u1 no tiene amigos en la red, listo, False 
                                         | (u1,u2) == r || (u2,u1) == r                  = True                                          -- Si r (Relacion) es direactamente la relacion de u1 y u2 True
                                         | esParte u1 r && (tieneAmigos u' rs)           = existeSecuenciaDeAmigosAux rs u' u2           -- Si u1 es parte de esta relacion y u' tiene amigos en la red busco llegar a u2 con u'
-                                        | esParte u1 r && not (tieneAmigos u' rs)       = existeSecuenciaDeAmigosAux rs u1 u2           -- Si u' (el relacionado con u1, que no es u2), no tiene amigos en la red, veo si hay otra relacion que contenga a u1
+                                        | esParte u1 r && not (tieneAmigos u' rs)       = existeSecuenciaDeAmigosAux rs u1 u2           -- Si u' (el relacionado con u1, que no es u2), no tiene amigos en la red, veo si hay otra relacion que contenga a u1  
                                         | otherwise                                     = existeSecuenciaDeAmigosAux (rs ++ [r]) u1 u2  -- si u1 no es parte de la relacion, pongo esa relacion al final de la cadena y pruebo con la siguiente         
                                         where u'        | fst r == u1 = snd r
                                                         | snd r == u1 = fst r
@@ -173,51 +175,3 @@ esParte u r     | fst r == u = True
 
 ------------------------------------------------------------------------------------------------------------------------
 
--- Ejemplos
-
-usuario1 = (1, "Juan")
-usuario2 = (2, "Natalia")
-usuario3 = (3, "Pedro")
-usuario4 = (4, "Mariela")
-usuario5 = (5, "Natalia")
-
-relacion1_2 = (usuario1, usuario2)
-relacion1_3 = (usuario1, usuario3)
-relacion1_4 = (usuario4, usuario1) -- Notar que el orden en el que aparecen los usuarios es indistinto
-relacion2_3 = (usuario3, usuario2)
-relacion2_4 = (usuario2, usuario4)
-relacion3_4 = (usuario4, usuario3)
-relacion4_5 = (usuario4, usuario5)
-relacion3_5 = (usuario3, usuario5)
-
-publicacion1_1 = (usuario1, "Este es mi primer post", [usuario2, usuario4])
-publicacion1_2 = (usuario1, "Este es mi segundo post", [usuario4])
-publicacion1_3 = (usuario1, "Este es mi tercer post", [usuario2, usuario5])
-publicacion1_4 = (usuario1, "Este es mi cuarto post", [])
-publicacion1_5 = (usuario1, "Este es como mi quinto post", [usuario5])
-
-publicacion2_1 = (usuario2, "Hello World", [usuario4])
-publicacion2_2 = (usuario2, "Good Bye World", [usuario1, usuario4])
-
-publicacion3_1 = (usuario3, "Lorem Ipsum", [])
-publicacion3_2 = (usuario3, "dolor sit amet", [usuario2])
-publicacion3_3 = (usuario3, "consectetur adipiscing elit", [usuario2, usuario5])
-
-publicacion4_1 = (usuario4, "I am Alice. Not", [usuario1, usuario2])
-publicacion4_2 = (usuario4, "I am Bob", [])
-publicacion4_3 = (usuario4, "Just kidding, i am Mariela", [usuario1, usuario3])
-
-
-usuariosA = [usuario1, usuario2, usuario3, usuario4]
-relacionesA = [relacion1_2, relacion1_4, relacion2_3, relacion2_4, relacion3_4]
-publicacionesA = [publicacion1_1, publicacion1_2, publicacion2_1, publicacion2_2, publicacion3_1, publicacion3_2, publicacion4_1, publicacion4_2]
-redA = (usuariosA, relacionesA, publicacionesA)
-
-usuariosB = [usuario1, usuario2, usuario3, usuario5]
-relacionesB = [relacion1_2, relacion2_3]
-publicacionesB = [publicacion1_3, publicacion1_4, publicacion1_5, publicacion3_1, publicacion3_2, publicacion3_3]
-redB = (usuariosB, relacionesB, publicacionesB)
-
-publicacionesC = [publicacion3_3, publicacion1_3]
-relacionesC = [relacion1_2, relacion2_3, relacion4_5]
-redC = (usuariosB, relacionesC, publicacionesC)
